da13330d99382ef3476a5aa002880aa13a97ee16
pitas1121 <150827958+pitas1121@users.noreply.github.com>
2025-02-09 23:26:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('dedicationButton').addEventListener('click', function() {
+    const dedicationText = document.getElementById('dedicationText');
+    dedicationText.classList.remove('hidden');
+});
